19cfee90ea11275b4e137a60af28cc2674dd021c
Oscip <oscar@peachy.ch>
2025-08-10 15:44:24 +0700

    Style: Embedded Layout adjusted
    - More appealing Layout

--- src/components/SoundcloudEmbedded.jsx
+++ src/components/SoundcloudEmbedded.jsx
@@ -1,18 +1,17 @@
 export default function SoundcloudEmbedded() {
     return(
-        <div>
-            <div>
-                <iframe
-                    width="100%"
-                    height="300"
-                    scrolling="no"
-                    frameBorder="no"
-                    allow="autoplay"
-                    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/545610837&color=%2384beff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
-                </iframe>
+        <div className="sound-container">
+            <iframe
+                width="95%"
+                height="300"
+                scrolling="no"
+                frameBorder="no"
+                allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/545610837&color=%2384beff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+            </iframe>
 
-            </div>
         </div>
+
     )
         ;
 }
