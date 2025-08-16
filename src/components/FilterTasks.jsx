c05a88f84b1c5a01ddd29ab3be104b73f7bd6b08
Oscip <oscar@peachy.ch>
2025-08-16 15:44:16 +0200

    Feat: Dropdown appears on arrowclick
    - Recognition of click on arrow
    - Creation of a dropdown
    - Appearance of the dropdown

--- src/components/FilterTasks.jsx
+++ src/components/FilterTasks.jsx
@@ -1,19 +1,64 @@
+import {useState} from "react";
 export default function FilterTasks() {
+
+    const [currentCategory, setCurrentCategory] = useState("");
+
+
+
+    const categoryCreator = () => {
+        const selectCategory = document.createElement("select");
+        selectCategory.id = "select-category";
+
+        const defaultOption = document.createElement("option");
+        defaultOption.text = "Category"
+
+        const homeOption = document.createElement("option");
+        homeOption.text = "Home";
+
+        const schoolOption = document.createElement("option");
+        schoolOption.text = "School";
+
+        const freeTimeOption = document.createElement("option");
+        freeTimeOption.text = "Free Time";
+
+        const parentDiv = document.getElementById("category-div");
+
+        const categoryParagraph = document.getElementById("category-paragraph");
+
+        const createCategoryParagraph = document.createElement("p");
+        createCategoryParagraph.text = "Category";
+        createCategoryParagraph.className = "task-header-text";
+
+        const categoryArrow = document.getElementById("category-arrow");
+
+        selectCategory.add(defaultOption);
+        selectCategory.add(homeOption);
+        selectCategory.add(schoolOption);
+        selectCategory.add(freeTimeOption);
+        parentDiv.removeChild(categoryParagraph);
+        parentDiv.insertBefore(selectCategory, categoryArrow);
+
+        console.log("category created");
+    }
+
     return (
         <>
             <div className="div-width">
                 <div className="task-header">
                     <div className="left-panel">
-                        <p className="task-header-text">Completed</p>
+                        <div className="arrow-filter-container">
+                            <p className="task-header-text">Completed</p>
+                            <div className="select-arrow-filter" id="completed-arrow"></div>
+                        </div>
                     </div>
                     <div className="toDoItemContent">
                         <div className="arrow-filter-container">
                             <p className="task-header-text">Description</p>
                             <div className="select-arrow-filter"></div>
                         </div>
-                        <div className="arrow-filter-container-category">
-                            <p className="task-header-text">Category</p>
-                            <div className="select-arrow-filter"></div>
+                        <div className="arrow-filter-container-category" id="category-div">
+                            <p className="task-header-text" id="category-paragraph">Category</p>
+                            <div className="select-arrow-filter" id="category-arrow" onClick={categoryCreator}></div>
                         </div>
                     </div>
                 </div>
